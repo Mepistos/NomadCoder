bc3929b2ed6dee257572945c0a8f7151fad6999e
Mepistos <apsxh65@gmail.com>
2022-09-07 06:13:34 +0900

    002. clock done

--- Vanila_JavaScript_Momentom/js/greeting.js
+++ Vanila_JavaScript_Momentom/js/greeting.js
@@ -1,4 +1,4 @@
-// 001. login
+// 001. login - greeting
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
