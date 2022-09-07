ae1e29d02e5563b47672b29dfe743e2e980e2163
Mepistos <apsxh65@gmail.com>
2022-09-07 19:36:12 +0900

    vanila js done

---
+++ Vanila_JavaScript_Momentom/js/weather.js
@@ -0,0 +1,22 @@
+// 005. weather
+// need: openweathermap.org 's API KEY
+const weather = document.querySelectorAll("#weather span:first-child");
+const city = document.querySelectorAll("#weather span:last-child");
+const API_KEY = "38d8e7c53b15270d86408120139df312"
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
